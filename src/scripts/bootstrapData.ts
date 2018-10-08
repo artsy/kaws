f61af9344faea6f64900bdbbeeae636cb4c6069a
Luc Succes <l2succes@gmail.com>
2018-10-08 19:22:52 -0400

    Adds fixtures/collections.json

--- src/scripts/bootstrapData.ts
+++ src/scripts/bootstrapData.ts
@@ -1,7 +1,8 @@
 import slugify from "slugify"
-import { Connection, createConnection, getMongoManager } from "typeorm"
-import { Collection, CollectionQuery, entities, Image } from "../Entities"
-import * as data from "../fixtures/CollectionsResolver.json"
+import { Connection, createConnection, getMongoRepository } from "typeorm"
+import { Collection, entities } from "../Entities"
+
+const data = require("../../fixtures/collections.json")
 
 async function bootstrap() {
   try {
@@ -14,34 +15,10 @@ async function bootstrap() {
       synchronize: true,
     })
 
-    const collection: Collection = new Collection()
-    collection.title = "KAWS, Companions"
-    collection.keywords = `Companion, BFF, Passing Through, Astroboy, Astro Boy, Small Lie,
-    Together, Accomplice, Twins, Stormtrooper,
-    Boba Fett, Darth Vader, Undercover Bear, Tweety, Pinocchio,
-    Resting Place, Along the Way, Final Days`
-    collection.description = `
-  Brian Donnelly, better known as KAWS, spent the first year of his career as an animator for Disney.
-  After leaving in 1997, KAWS took inspiration from the company’s signature cartoon, Mickey Mouse,
-  to create his own set of characters that he named “Companions.” With gloved hands and X’s for eyes,
-  “Companions” first appeared in KAWS’s graffiti works across New York City in the late 1990s.
-  By the end of the decade, the street artist created his first three-dimensional version,
-  and his characters have since taken on a variety of colors, sizes, and poses.
-  In 2017, his four-foot-tall Seated Companion (2011) broke the auction record for the series,
-  selling for over $400,000. However, many of KAWS’s “Companions” are considerably more affordable,
-  such as his vinyl toys produced in collaboration with esteemed manufacturers including Bounty Hunter,
-  Bape, Medicom, and his own brand OriginalFake, which was active between 2006 and 2013.
-  `
-    collection.slug = slugify(collection.title)
-    collection.headerImage = new Image()
-    collection.headerImage.small = collection.headerImage.medium = collection.headerImage.large =
-      "https://cdn3.pitchfork.com/longform/239/kanyeart1865.jpg"
+    const repository = getMongoRepository(Collection)
+    const collections = await repository.create(data as any)
 
-    collection.query = new CollectionQuery()
-    collection.query.artist_ids = ["kaws"]
-
-    const manager = await getMongoManager()
-    await manager.save(collection)
+    await repository.save(collections)
   } catch (err) {
     // tslint:disable-next-line
     console.error(err)
