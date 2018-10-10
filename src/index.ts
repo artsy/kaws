40479ba52aaae4a735059f9b7fed9ada29ef62ec
Luc Succes <l2succes@gmail.com>
2018-10-10 15:04:43 -0400

    [TypeORM] switch to mongodb url for config

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { Connection, createConnection } from "typeorm"
 import { entities } from "./Entities"
 import { CollectionsResolver } from "./Resolvers/Collections"
 
+const { MONGOHQ_URL } = process.env
+
 async function bootstrap() {
   const schema = await buildSchema({
     resolvers: [CollectionsResolver],
@@ -15,9 +17,7 @@ async function bootstrap() {
 
   const connection: Connection = await createConnection({
     type: "mongodb",
-    host: "localhost",
-    port: 27017,
-    database: "kaws",
+    url: MONGOHQ_URL,
     entities,
     extra: {
       useNewUrlParser: true,
