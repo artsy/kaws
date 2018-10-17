3fe9558d615724e02bc75c1ac9d3bd2ae90d7c45
Luc Succes <l2succes@gmail.com>
2018-10-17 11:25:39 -0400

    Fixes mongoDB Config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 
 import { GraphQLServer, Options } from "graphql-yoga"
+import { parse } from "mongodb-uri"
 import * as morgan from "morgan"
 import { Connection, createConnection } from "typeorm"
 import { createSchema } from "./createSchema"
@@ -8,33 +9,50 @@ import { entities } from "./Entities"
 
 const { MONGOHQ_URL, NODE_ENV, PORT } = process.env
 
+// tslint:disable:no-console
+
 async function bootstrap() {
   const schema = await createSchema()
   const server = new GraphQLServer({ schema })
   const app = server.express
 
-  const connection: Connection = await createConnection({
-    type: "mongodb",
-    url: MONGOHQ_URL,
-    entities,
-  })
+  try {
+    const { username, password, database, hosts, options } = parse(MONGOHQ_URL!)
 
-  const serverOptions: Options = {
-    port: PORT,
-    endpoint: "/graphql",
-    playground: "/playground",
-    debug: NODE_ENV === "development",
+    const connection: Connection = await createConnection({
+      type: "mongodb",
+      username,
+      password,
+      database,
+      ...options,
+      host: hosts.map(a => a.host).join(","),
+      port: 27017,
+      ssl: true,
+      entities,
+    })
+    if (connection.isConnected) {
+      console.log("Successfully connected to MongoDB database:", database)
+    }
+
+    const serverOptions: Options = {
+      port: PORT,
+      endpoint: "/graphql",
+      playground: "/playground",
+      debug: NODE_ENV === "development",
+    }
+
+    app.get("/health", (req, res) => {
+      return res.status(200).end()
+    })
+    app.use(morgan("combined"))
+
+    server.start(serverOptions, ({ port, playground }) => {
+      // tslint:disable-next-line
+      console.log(`Server is running, GraphQL Playground available at http://localhost:${port}${playground}`)
+    })
+  } catch (e) {
+    console.error("Failed to connect to MongoDB ", e)
   }
-
-  app.get("/health", (req, res) => {
-    return res.status(200).end()
-  })
-  app.use(morgan("combined"))
-
-  server.start(serverOptions, ({ port, playground }) => {
-    // tslint:disable-next-line
-    console.log(`Server is running, GraphQL Playground available at http://localhost:${port}${playground}`)
-  })
 }
 
 bootstrap()
