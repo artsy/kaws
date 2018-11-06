5079201733c7dab8de620b77987aac5ff1236735
Eve Essex <eve.essex@gmail.com>
2018-11-06 17:10:01 -0500

    can query for categories to receive collections grouped by category

--- src/Resolvers/Collections.ts
+++ src/Resolvers/Collections.ts
@@ -1,6 +1,7 @@
 import { Arg, Query, Resolver } from "type-graphql"
 import { getMongoRepository } from "typeorm"
 import { Collection } from "../Entities/Collection"
+import { CollectionCategory } from "../Entities/CollectionCategory"
 
 @Resolver(of => Collection)
 export class CollectionsResolver {
@@ -10,6 +11,30 @@ export class CollectionsResolver {
   @Query(returns => [Collection])
   async collections(): Promise<Collection[]> {
     return await this.repository.find()
+  }
+
+  // TODO: should return a connection
+  @Query(returns => [CollectionCategory])
+  async categories(): Promise<CollectionCategory[]> {
+    const data = await this.repository.group(
+      { category: 1 },
+      { category: { $exists: true } },
+      {},
+      (curr, result) => {
+        if (!result.collections) {
+          result.collections = [curr]
+        } else {
+          result.collections.push(curr)
+        }
+      },
+      result => result,
+      true
+    )
+
+    return data.map(({ category, collections }) => ({
+      name: category,
+      collections,
+    }))
   }
 
   @Query(returns => Collection, { nullable: true })
