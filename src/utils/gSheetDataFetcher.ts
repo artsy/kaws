53dff508edadfc06aef29c0f9ce6fa92dee89367
Luc Succes <l2succes@gmail.com>
2019-03-28 17:25:44 -0400

    Switch to JWT

--- src/utils/gSheetDataFetcher.ts
+++ src/utils/gSheetDataFetcher.ts
@@ -1,19 +1,50 @@
 import { google } from "googleapis"
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
+const { GOOGLE_JWT } = process.env
+const credentials = require("../../gcloud-credentials.json")
 
 // https://developers.google.com/sheets/api/quickstart/nodejs?authuser=1
 
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
-export const gSheetDataFetcher = (sheetID: string) => {
-  const oAuth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET
+export const gSheetDataFetcher = async (sheetID: string) => {
+  console.log("jwt", GOOGLE_JWT!)
+  const jwtClient = await authorize()
+  const sheets = google.sheets({ version: "v4", auth: jwtClient })
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1_lyvVK8Bz-qt3l_kcR2-2xbfr9lz_q3i1G9W77sTcvQ",
+      range: "'Batch 10'",
+    },
+    (err, res) => {
+      if (err) {
+        return console.log("The API returned an error: " + err)
+      }
+      const rows = res.data.values
+      if (rows.length) {
+        // console.log(rows)
+      } else {
+        console.log("No data found.")
+      }
+    }
   )
+}
 
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
+const authorize = () => {
+  return new Promise((resolve, reject) => {
+    const jwtClient = new google.auth.JWT(
+      credentials.client_email,
+      "",
+      credentials.private_key,
+      ["https://www.googleapis.com/auth/spreadsheets"]
+    )
+    jwtClient.authorize((err, tokens) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      } else {
+        resolve(jwtClient)
+      }
+    })
   })
 }
+
+gSheetDataFetcher("")
