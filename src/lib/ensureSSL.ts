b28d90a71e41fcaf0a6f932c1990746a3b1f229e
xtina-starr <c.thompsonms@gmail.com>
2019-09-11 16:20:44 -0400

    fix comments

--- src/lib/ensureSSL.ts
+++ src/lib/ensureSSL.ts
@@ -5,11 +5,10 @@ const { APP_URL } = process.env
 
 export const ensureSSL = (req, res, next) => {
   /*
-  * req.secure is a boolean that return if the request protocol is 'https'
+  *
+  * req.secure is a boolean that return true if the request protocol is 'https'
   * https://expressjs.com/en/api.html#req.secure
   * 
-  * APP_URL is parsed because req.secure does not work local. APP_URL determines 
-  * whether or not we need to make the requestion via https
   */
   if (!req.secure) {
     res.redirect(301, APP_URL + req.url)
