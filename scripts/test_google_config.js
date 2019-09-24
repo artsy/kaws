0161e43bf63bbd62bd14c77bda9cfc3ad82215bd
mykola <mbilokonsky@gmail.com>
2019-09-24 15:43:14 -0400

    updated readme, cleaned up test code

--- scripts/test_google_config.js
+++ scripts/test_google_config.js
@@ -1,73 +1,75 @@
+require("dotenv").config()
 const { google } = require("googleapis")
 const color = require('bash-color')
-require("dotenv").config()
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SPREADSHEET_IDS_ALLOWLIST } = process.env
+
+const FAILURE_REASONS = {
+  needs_id: `You must specify a ${color.yellow('GOOGLE_CLIENT_ID')}; this may be a numeric value or an email address of an account with access!`,
+  needs_secret: `You must specify a ${color.yellow('GOOGLE_CLIENT_SECRET')}; this should be in the form of a private SSH key, and should include \\n tokens on linebreaks.`,
+  needs_spreadsheet: `You must specify a ${color.yellow('SPREADSHEET_IDS_ALLOWLIST')} - this should be the ID of the spreadsheet you're trying to access.`,
+  authorization_failed: `The ${color.yellow('GOOGLE_CLIENT_ID')} and ${color.yellow('GOOGLE_CLIENT_SECRET')} you provided are being rejected. See console output for more information.`,
+  google_access: `Although ${color.green('authentication to google succeeded')}, the ${color.red('query to google sheets failed')}. See console output for more information.`
+}
 
 const reasons = []
 const fail = reasons => {
-  console.log("Unable to successfully interface with Google Sheets API given the following errors:")
-  reasons.forEach(reason => console.log(`\t- ${reason}`))
+  console.error("Unable to successfully interface with Google Sheets API given the following errors:")
+  reasons.forEach(reason => console.log(`\t- ${FAILURE_REASONS[reason]}`))
   process.exit(1)
 }
 
-if (!GOOGLE_CLIENT_ID) {
-  reasons.push(`You must specify a ${color.yellow('GOOGLE_CLIENT_ID')}; this may be a numeric value or an email address of an account with access!`)
+if (!GOOGLE_CLIENT_ID) { reasons.push('needs_id') }
+if (!GOOGLE_CLIENT_SECRET) { reasons.push('needs_secret') }
+if (!SPREADSHEET_IDS_ALLOWLIST) { reasons.push('needs_spreadsheet') }
+if (reasons.length > 0) { fail(reasons) }
+
+const config = {
+  id: GOOGLE_CLIENT_ID,
+  keyfile: "",
+  secret: GOOGLE_CLIENT_SECRET.replace(/\\n/g, "\n"),
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 }
 
-if (!GOOGLE_CLIENT_SECRET) {
-  reasons.push(`You must specify a ${color.yellow('GOOGLE_CLIENT_SECRET')}; this should be in the form of a private SSH key, and should include \\n tokens on linebreaks.`)
-}
-
-if (!SPREADSHEET_IDS_ALLOWLIST) {
-  reasons.push(`You must specify a ${color.yellow('SPREADSHEET_IDS_ALLOWLIST')} - this should be the ID of the spreadsheet you're trying to access.`)
-}
-
-if (reasons.length > 0) {
-  fail(reasons)
-}
-
-const getClient = () => {
+const runTest = () => {
   authorize()
     .then(client => {
       const sheets = google.sheets({ version: "v4", auth: client })
-      do_stuff(sheets)
+      confirmAccess(sheets)
     }).catch(e => {
-      reasons.push(e.message)
-      process.exit(1)
+      fail([...reasons, 'authorization_failed'])
     })
 }
 
 const authorize = () => {
-  return new Promise((resolve, reject) => {
-    const jwtClient = new google.auth.JWT(
-      GOOGLE_CLIENT_ID,
-      "",
-      GOOGLE_CLIENT_SECRET.replace(/\\n/g, "\n"),
-      ["https://www.googleapis.com/auth/spreadsheets"]
-    )
+  const promise = new Promise((resolve, reject) => {
+    const { id, keyfile, secret, scopes } = config
+    const jwtClient = new google.auth.JWT(id, keyfile, secret, scopes)
 
-    jwtClient.authorize((err, tokens) => {
-      if (err) {
-        console.log("Error authorizing!")
-        console.log(err)
-        reject(err)
-      } else {
-        resolve(jwtClient)
-      }
-    })
+    jwtClient
+      .authorize((err) => {
+        if (err) {
+          console.log('[ERROR]', err.message)
+          reject(err)
+        } else {
+          resolve(jwtClient)
+        }
+      })
   })
-    .catch(e => { throw (e) })
+
+  promise.catch(e => { throw (e) })
+  return promise
 }
 
-const do_stuff = sheets => {
+const confirmAccess = sheets => {
   sheets.spreadsheets.values.get({
     spreadsheetId: SPREADSHEET_IDS_ALLOWLIST,
     range: "debug"
   },
     (err, res) => {
       if (err) {
-        fail([`${color.red("error")}: ${err.message}`])
-        process.exit(1)
+        console.log('[ERROR]', err.message)
+        reasons.push('google_access')
+        fail([...reasons, 'google_access'])
       }
       const [_, ...data] = res.data.values
       console.log(`${color.green("Success!")} You have loaded the debug spreadsheet, which currently contains ${data.length} records.`)
@@ -76,4 +78,4 @@ const do_stuff = sheets => {
     })
 }
 
-getClient()
+runTest()
