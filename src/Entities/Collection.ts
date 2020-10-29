51d63a00e3cc6eb07d57cb71dbceb7b0d5713b43
Matthew Zikherman <mzikherman@gmail.com>
2020-10-29 17:24:08 -0400

    feat: remove internalID from schema

--- src/Entities/Collection.ts
+++ src/Entities/Collection.ts
@@ -6,13 +6,9 @@ import { CollectionQuery } from "./CollectionQuery"
 @ObjectType({ description: "Object representing a collection page" })
 @Entity()
 export class Collection {
-  @Field(type => ID)
+  @Field((type) => ID)
   @ObjectIdColumn()
   id: ObjectID
-
-  @Field(type => ID)
-  @ObjectIdColumn({ name: "id" })
-  internalID: ObjectID
 
   @Index({ unique: true })
   @Field({
@@ -41,14 +37,14 @@ export class Collection {
   @Column({ nullable: true })
   descriptionMarkdown?: string
 
-  @Field(type => String, {
+  @Field((type) => String, {
     nullable: true,
     description: "Background image for the header of the collection page",
   })
   @Column()
   headerImage?: string
 
-  @Field(type => String, {
+  @Field((type) => String, {
     nullable: true,
     description:
       "URL for Thumbnail image to be used when this collection is displayed.",
@@ -56,7 +52,7 @@ export class Collection {
   @Column()
   thumbnail?: string | null = null
 
-  @Field(type => String, {
+  @Field((type) => String, {
     description: "Set of keywords used for SEO purposes",
   })
   @Column()
@@ -69,14 +65,14 @@ export class Collection {
   @Column({ nullable: true })
   credit?: string
 
-  @Field(type => String, { description: "Category of the collection" })
+  @Field((type) => String, { description: "Category of the collection" })
   @Column()
   category: string
 
-  @Field(type => CollectionQuery, {
+  @Field((type) => CollectionQuery, {
     description: "Structured object used to build filtered artworks query",
   })
-  @Column(type => CollectionQuery)
+  @Column((type) => CollectionQuery)
   query: CollectionQuery
 
   @Field()
@@ -87,7 +83,7 @@ export class Collection {
   @Column()
   updatedAt: Date
 
-  @Field(type => Number, {
+  @Field((type) => Number, {
     nullable: true,
     description: "Suggested average price for included works",
     deprecationReason: "Prefer priceGuidance",
@@ -95,46 +91,46 @@ export class Collection {
   @Column({ nullable: true })
   price_guidance: number | null = null
 
-  @Field(type => Number, {
+  @Field((type) => Number, {
     nullable: true,
     description: "Suggested average price for included works",
   })
   @Column({ name: "price_guidance", nullable: true })
   priceGuidance: number | null = null
 
-  @Field(type => Boolean, {
+  @Field((type) => Boolean, {
     description: "Collection can be surfaced on editorial pages",
     deprecationReason: "Prefer showOnEditorial",
   })
   @Column({ default: false })
   show_on_editorial: boolean = false
 
-  @Field(type => Boolean, {
+  @Field((type) => Boolean, {
     description: "Collection can be surfaced on editorial pages",
   })
   @Column({ name: "show_on_editorial", default: false })
   showOnEditorial: boolean = false
 
-  @Field(type => Boolean, {
+  @Field((type) => Boolean, {
     description: "Collection has prioritized connection to artist",
     deprecationReason: "Prefer isFeaturedArtistContent",
   })
   @Column({ default: false })
   is_featured_artist_content: boolean = false
 
-  @Field(type => Boolean, {
+  @Field((type) => Boolean, {
     description: "Collection has prioritized connection to artist",
   })
   @Column({ name: "is_featured_artist_content", default: false })
   isFeaturedArtistContent: boolean = false
 
-  @Field(type => [CollectionGroup], {
+  @Field((type) => [CollectionGroup], {
     description: "CollectionGroups of this collection",
   })
-  @Column(type => CollectionGroup)
+  @Column((type) => CollectionGroup)
   linkedCollections: CollectionGroup[]
 
-  @Field(type => [String], {
+  @Field((type) => [String], {
     description:
       "IDs of artists that should be excluded from Featured Artists for this collection",
     nullable: true,
